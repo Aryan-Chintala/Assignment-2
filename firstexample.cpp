13101dc5b18cdef003bc33d8f47ef863417a840d
Aryan-Chintala <166235882+Aryan-Chintala@users.noreply.github.com>
2025-02-09 18:43:48 +0530

    Add files via upload

---
+++ firstexample.cpp
@@ -0,0 +1,35 @@
+/** 
+ * @file
+ * @author Jan Doe 
+ * @version 1.0 
+ * 
+ * @section LICENSE 
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License.
+ *
+ * @section DESCRIPTION
+ * 
+ * The Time class represents a moment in time. 
+ */ 
+
+class Time {
+public:
+    /**
+     * Constructor that sets the time to a given value.
+     *
+     * @param timemillis Number of milliseconds since Jan 1, 1970.
+     */
+    Time(int timemillis) {
+        // Implementation
+    }
+
+    /**
+     * Get the current time.
+     *
+     * @return A Time object set to the current time.
+     */
+    static Time now() {
+        // Implementation
+    }
+};
